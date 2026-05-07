8f30e7f86a5ded0479d9801972e0d5727c6cbbb0
agatha <rukwaroagatha7@gmail.com>
2026-05-07 17:56:32 +0300

    Fix API baseURL for production deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: false
 });
 
@@ -10,10 +10,7 @@ API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${token}`
-    };
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
